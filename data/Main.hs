eb6bdb5409b289d65da2ee2ec2d832a0df8dc01c
kozlowja <mail@jakub-kozlowski.com>
2014-03-04 23:33:59 +0000

    Generated the ADT for the ITCH message, without the fields.

--- data/Main.hs
+++ data/Main.hs
@@ -6,18 +6,16 @@ import Data.Text (Text)
 import Data.String
 import Data.Word (Word8)
 import Control.Applicative ((<$>), (<*>), (<*), (<|>), pure)
-import Control.Monad
+import Control.Monad ((=<<))
 import Data.Char (toLower)
 import Data.Map (Map)
-import Data.List (intercalate, partition, stripPrefix, sortBy)
 import qualified Data.Map as M
 import Data.Maybe (catMaybes)
-import Debug.Trace (trace)
 import qualified Text.XML as XML
 import Text.XML (Node, Element, elementAttributes)
-import Text.XML.Cursor (fromNode, node, attribute, fromDocument, child, element, descendant, content,  ($/), (&//), (&/), (&|))
+import Text.XML.Cursor (fromNode, node, attribute, fromDocument, child, element, ($/), (&//), (&/), (&|))
 import qualified Data.ByteString as B
-import Language.Haskell.Exts ()
+import Language.Haskell.Exts hiding (parseType)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
@@ -74,24 +72,72 @@ parseType attrs "Price" = Price <$> lookupRead "length" attrs
                                 <*> lookupRead "decimal_points" attrs
 parseType _ "BitField"  = Just BitField
 parseType attrs "Alpha" = Alpha <$> lookupRead "length" attrs
-parseType attrs "Date"  = Date <$> lookupRead "length" attrs
-parseType attrs "Time"  = Time <$> lookupRead "length" attrs
+parseType attrs "Date"  = Date  <$> lookupRead "length" attrs
+parseType attrs "Time"  = Time  <$> lookupRead "length" attrs
 parseType _ _           = Nothing
+
+srcLoc :: SrcLoc
+srcLoc = SrcLoc {srcFilename = "foo.hs", srcLine = 1, srcColumn = 1}
+
+fixName :: Text -> Text
+fixName = T.replace "ITCH" "" . T.replace " " ""
+
+recordDecl :: Message -> ConDecl
+recordDecl (Message name _ fields) = ConDecl ident []
+  where ident        = Ident (T.unpack (fixName name))
+        --args         = map x fields
+        --grp fty      = UnBangedTy $ strongFIXTyCon fty
+        --ty True fty  = UnBangedTy $ strongFIXTyCon fty
+        --ty False fty = unbangedMaybeFIXTyCon fty
+        --x (g@Group{}, _)   = grp g
+        --x (f@Field{}, reqd)= ty reqd f
+
+generateMessageConDecl :: Message -> QualConDecl
+generateMessageConDecl msg = QualConDecl srcLoc tyVarBind context ctor
+  where tyVarBind = []
+        context   = []
+        ctor      = recordDecl msg
+
+generateMessageDecl :: [Message] -> Decl
+generateMessageDecl msgs = decl where
+  decl      = DataDecl srcLoc DataType context name' tyVarBind decls derived
+  decls     = map generateMessageConDecl msgs
+  name'     = Ident "ITCHMessage"
+  context   = []
+  tyVarBind = []
+  derived   = map ((\v -> (v, [])) . UnQual . Ident) ["Generic","Show","Eq"]
+
+generateMessageModule :: [Message] -> Module
+generateMessageModule msgs = Module srcLoc modName pragmas warningText exports imports decls
+  where modName = ModuleName $ "Data.ITCH"
+        pragmas = []
+        warningText = Nothing
+        exports = Nothing
+        imports = []
+        decls = messagesDecl
+        messagesDecl  = [generateMessageDecl msgs]
 
 main :: IO ()
 main = do
    args <- getArgs
    if length args /= 1
-    then do putStrLn "Usage: Main <Path-to-xml>"
-            return ()
-    else do let path = args !! 0
-            -- Get the cursor
-            document <- XML.readFile XML.def (fromString path)
-            let cursor = fromDocument document
+    then do
+      putStrLn "Usage: Main <Path-to-xml>"
+      return ()
+    else do
+      let path = args !! 0
+      -- Get the cursor
+      document <- XML.readFile XML.def (fromString path)
+      let cursor = fromDocument document
 
-            -- Start descending
-            print $ cursor $/ element "msgs"
-                           &/ element "msg" &| (onElement parseMessage) . node
+          -- Parse messages
+          messages = catMaybes $ cursor $/ element "msgs"
+                               &/ element "msg" &| (onElement parseMessage) . node
 
-            return ()
+          types = generateMessageModule messages
+          ppr = prettyPrintStyleMode style defaultMode
+
+      putStrLn (ppr types)
+      print messages
+      return ()
 
