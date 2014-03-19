130da21aa14db7dd57e8968f6ad267a39ca0a259
kozlowja <mail@jakub-kozlowski.com>
2014-03-19 20:36:38 +0000

    Added tests for extra padding.

--- tests/Data/ITCH/ITCH51Test.hs
+++ tests/Data/ITCH/ITCH51Test.hs
@@ -15,6 +15,7 @@ import qualified Data.Binary.Get       as Get
 import qualified Data.ByteString.Lazy  as L
 import Data.Either
 import qualified Data.ITCH.ITCH51      as ITCH
+import qualified Data.ITCH.Types       as Types
 import           Test.Tasty
 import           Test.Tasty.QuickCheck as QC
 
@@ -25,6 +26,7 @@ qcProps = testGroup "(checked by QuickCheck)"
   [ QC.testProperty "msg == msg" eqTest
   , QC.testProperty "decode (encode msg) == msg" serialiseDeserialise
   , QC.testProperty "block of messages should deserialise cleanly" serialiseDeserialiseBlock
+  , QC.testProperty "block of messages with modified sizes should deserialise cleanly" serialiseDeserialiseBlockDifferentLengths
   ]
 
 eqTest :: ITCH.ITCHMessage -> Bool
@@ -38,9 +40,26 @@ serialiseDeserialise msg = let serialised   = B.encode msg
 serialiseDeserialiseBlock :: [ITCH.ITCHMessage] -> Bool
 serialiseDeserialiseBlock msgs = deserialised == msgs
   where serialised = L.concat $ map B.encode msgs
-        deserialised = map (\(_, _, msg) -> msg) (rights (singleFold serialised))
-        singleFold b
+        deserialised = deserialiseBlock serialised
+
+serialiseDeserialiseBlockDifferentLengths :: [ITCH.ITCHMessage] -> Bool
+serialiseDeserialiseBlockDifferentLengths msgs = deserialised == msgs
+  where serialised = L.concat $ map (padMessage 100 . B.encode) msgs
+        deserialised = deserialiseBlock serialised
+
+deserialiseBlock :: L.ByteString -> [ITCH.ITCHMessage]
+deserialiseBlock bs = map (\(_, _, msg) -> msg) (rights (singleFold bs))
+  where singleFold b
           | L.null b  = []
           | otherwise = case (Get.runGetOrFail (B.get :: Get.Get ITCH.ITCHMessage) b) of
                         l@(Left (remaining, _, _)) -> [l] ++ (singleFold remaining)
                         r@(Right (remaining, _, newMsg)) -> [r] ++ (singleFold remaining)
+
+-- | Pads the message on the right with additional bytes
+--  and updates the message length.
+padMessage :: Int -> L.ByteString -> L.ByteString
+padMessage padding bs =
+  let curLength = Get.runGet Types.getMessageLength bs
+      newLength = curLength + (fromIntegral padding)
+      paddingBs = L.replicate (fromIntegral padding) 0x00
+  in (newLength `L.cons` (L.tail bs)) `L.append` paddingBs
