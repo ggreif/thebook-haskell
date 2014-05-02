23e30b74240714d59957e1af27892dcc4a31255a
Jakub Kozlowski <mail@jakub-kozlowski.com>
2014-05-02 22:09:41 +0100

    Added a test autorun in .ghci, so awesome. Started adding new tests to MonadTest.

--- tests/Data/TheBook/MonadTest.hs
+++ tests/Data/TheBook/MonadTest.hs
@@ -10,6 +10,7 @@
 -----------------------------------------------------------------------------
 module Data.TheBook.MonadTest (tests) where
 
+import           Control.Monad         (mzero)
 import           Data.TheBook.Monad
 import           Data.TheBook.Types    as Types
 import           Test.Tasty
@@ -21,6 +22,8 @@ tests = testGroup "Data.TheBook.MonadTest" [qcProps]
 qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)"
   [ QC.testProperty "return in Monad should match" prop_return_should_match
+  , QC.testProperty ">>= in Monad should stop at NoMatch" prop_bind_should_stop_at_no_match
+  , QC.testProperty "dummy" prop
   ]
 
 type TestRule   a = Rule Int [String] String a
@@ -34,8 +37,18 @@ isMatch s w a r = case r of
   Match s'' w' a' -> s == s'' && w == w' && a == a'
   _              -> False
 
+isNoMatch :: Result s w e a -> Bool
+isNoMatch NoMatch = True
+isNoMatch _       = False
+
 prop_return_should_match :: Int -> Bool
-prop_return_should_match i = isMatch s' [] i (runRule retI s')
-  where
-    retI :: TestRule Int
-    retI = return i
+prop_return_should_match i = isMatch s' [] i (runRule rule s')
+  where rule :: TestRule Int
+        rule = return i
+
+prop_bind_should_stop_at_no_match :: Bool
+prop_bind_should_stop_at_no_match = isNoMatch (runRule rule s')
+  where rule :: TestRule Int
+        rule = mzero
+
+prop = True
